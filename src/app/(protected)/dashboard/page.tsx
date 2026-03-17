da4948d325df0e3eb3495b64e8d5086fc6b67950
mcqeems <mcqeemsofficial@gmail.com>
2026-03-17 16:04:11 +0700

    fix dashboard layout

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -51,31 +51,37 @@ export default function Dashboard() {
       {/* Inside Dashbord */}
       <section className="space-x-2 space-y-2 py-2">
         {/* Location */}
-        <div className="flex flex-row gap-2 p-2 bg-accent/50 w-full">
-          <div className="w-full max-w-full py-2 bg-primary font-medium">
-            Provinsi:
-          </div>
-          <div className="w-full max-w-full py-2 bg-primary font-medium">
-            Kabupaten:
-          </div>
-          <div className="w-full max-w-full py-2 bg-primary font-medium">
-            Kecamatan:
-          </div>
-          <div className="w-full max-w-full py-2 bg-primary font-medium">
-            Kelurahan:
-          </div>
+        <div className="flex flex-row gap-2 p-2 w-full rounded-lg border border-border">
+          <div className="w-full max-w-full py-2 font-medium">Provinsi:</div>
+          <div className="w-full max-w-full py-2 font-medium">Kabupaten:</div>
+          <div className="w-full max-w-full py-2 font-medium">Kecamatan:</div>
+          <div className="w-full max-w-full py-2 font-medium">Kelurahan:</div>
         </div>
+        {/* Tops */}
         <div className="flex md:flex-row flex-col gap-2 w-full">
           {/* Left Side */}
-          <div className="flex flex-col w-full max-w-full md:max-w-lg bg-accent/50 gap-2">
-            <div className="w-full h-[150px] bg-primary"></div>
-            <div className="w-full h-[300px] bg-primary"></div>
-            <div className="w-full h-[150px] bg-primary"></div>
+          <div className="flex flex-col w-full max-w-full md:max-w-lg gap-2">
+            <div className="w-full h-[125px] bg-primary">Cuaca Hari ini</div>
+            <div className="w-full h-[300px] bg-primary">
+              Prakiraan Cuaca selama 3 hari
+            </div>
+            <div className="w-full h-[125px] bg-primary">
+              Kabar Gempa bumi terbaru
+            </div>
           </div>
           {/* Right Side */}
-          <div className="flex flex-col w-full max-w-full bg-accent/50 gap-2">
-            <div className="w-full h-full bg-primary"></div>
-            <div className="w-full h-full bg-primary"></div>
+          <div className="flex flex-col w-full max-w-full gap-2">
+            <div className="w-full h-full bg-primary">Map</div>
+            <div className="w-full h-full bg-primary">AI Analysis</div>
+          </div>
+        </div>
+        {/* Bottoms */}
+        <div className="flex md:flex-row flex-col gap-2 w-full">
+          <div className="h-[150px] w-full md:max-w-[50%] max-w-full bg-primary">
+            Berita peringatan cuaca
+          </div>
+          <div className="h-[150px] w-full md:max-w-[50%] max-w-full bg-primary">
+            Berita 15 gempa terakhir dengan magnitudo 5+
           </div>
         </div>
       </section>
