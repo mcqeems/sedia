a2fbacacd913d75132d206d9dd9f1cd2be3851bf
mcqeems <mcqeemsofficial@gmail.com>
2026-03-20 18:11:10 +0700

    added initial context

--- src/context/dashContext.tsx
+++ src/context/dashContext.tsx
@@ -1,12 +1,47 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import type React from "react";
+import { createContext, useContext, useReducer } from "react";
 
-type DashboardContents = {
-  displayLocation: string;
-  adm4: string;
-  latitude: string;
-  longitude: string;
+interface DashboardContents {
+  displayLocation: string | undefined;
+  adm4: string | undefined;
+  latitude: string | undefined;
+  longitude: string | undefined;
+}
+
+interface DashboardStates {
+  state: DashboardContents;
+  status:
+    | "idle"
+    | "loading-weather"
+    | "loading-prediction"
+    | "loading-earthquake"
+    | "loading-weather-warning"
+    | "loading-earthquake-news"
+    | "loading-map"
+    | "loading-ai"
+    | "loading-all";
+}
+
+const defaultDash: DashboardStates = {
+  state: {
+    displayLocation: undefined,
+    adm4: undefined,
+    latitude: undefined,
+    longitude: undefined,
+  },
+  status: "loading-all",
 };
 
-export default function dashContext() {}
+const DashContext = createContext<DashboardStates>(defaultDash);
+
+export default function dashContext({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  <DashContext value={defaultDash}>{children}</DashContext>;
+}
+
+export const useDashContext = () => useContext(DashContext);
