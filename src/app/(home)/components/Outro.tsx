240d89e317a2d520e7b9967aa64a4d7c3d60cc0b
mcqeems <mcqeemsofficial@gmail.com>
2026-03-28 14:35:28 +0700

    add animation for outro.tsx

--- src/app/(home)/components/Outro.tsx
+++ src/app/(home)/components/Outro.tsx
@@ -6,7 +6,18 @@ import Image from "next/image";
 export default function Outro() {
   return (
     <section className="px-2 pb-8 pt-16 md:px-1 md:pt-24 w-full">
-      <div className="relative rounded-2xl overflow-hidden px-2 py-12 bg-primary">
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          damping: 12,
+          duration: 0.8,
+        }}
+        viewport={{ once: true }}
+        className="relative rounded-2xl overflow-hidden px-2 py-12 bg-primary"
+      >
         <div className="flex flex-col gap-2 justify-center items-center z-2 text-background text-center">
           <h2 className="text-4xl max-w-3xl drop-shadow-lg/15 z-2">
             Sudah siap menghadapi perubahan cuaca dan potensi bencana?
@@ -16,6 +27,9 @@ export default function Outro() {
             resiko bencana.
           </p>
           <motion.a
+            whileHover={{ scale: 1.05, backgroundColor: "#4a83fe" }}
+            whileTap={{ scale: 0.9, backgroundColor: "#3b61b1" }}
+            transition={{ type: "spring", stiffness: 300, damping: 15 }}
             href="/dashboard"
             className="z-2 mt-8 bg-primary px-6 py-2 rounded-full cursor-pointer"
           >
@@ -24,14 +38,14 @@ export default function Outro() {
         </div>
         <div className="absolute inset-0 z-1 pointer-events-none">
           <Image
-            src="/blue-ambient-outro-2.jpg"
+            src="/silky-background.jpg"
             alt="background card"
             fill
             className="object-cover object-center opacity-75"
             sizes="(min-width: 768px) 50vw, 100vw"
           />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
