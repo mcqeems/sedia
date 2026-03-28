fdead7d8336fdfa922dcf97bb3eb449759c027c2
mcqeems <mcqeemsofficial@gmail.com>
2026-03-28 17:45:17 +0700

    change siaga to sedia at homepage metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,7 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 
 export const metadata: Metadata = {
   title: {
-    absolute: "Siaga",
+    absolute: "Sedia",
   },
   description:
     "Sedia adalah aplikasi pemantauan cuaca dan informasi kebencanaan untuk membantu masyarakat lebih siap menghadapi kondisi ekstrem.",
