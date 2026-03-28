7b3229c136f18c65a363a20bbef93a7b8155febd
mcqeems <mcqeemsofficial@gmail.com>
2026-03-28 10:45:31 +0700

    add responsiveness at fitur.tsx

--- src/app/(home)/components/Fitur.tsx
+++ src/app/(home)/components/Fitur.tsx
@@ -52,7 +52,7 @@ export default function Fitur() {
       </div>
 
       <div className="flex flex-col md:flex-row gap-2 md:gap-4 md:mt-12 mt-8">
-        <div className="relative flex-1 py-20 px-8 flex flex-col gap-4 justify-center items-center rounded-2xl overflow-hidden ">
+        <div className="relative flex-1 md:py-20 md:px-8 py-8 px-4 flex flex-col gap-4 justify-center items-center rounded-2xl overflow-hidden ">
           <div className="absolute inset-0 z-[1] pointer-events-none">
             <Image
               src="/bg-card.png"
@@ -71,12 +71,21 @@ export default function Fitur() {
             Selalu selangkah lebih maju untuk meminimalkan risiko dan melindungi
             diri Anda.
           </p>
+
           <Image
             src="/exclamation-mark-glass.png"
             alt="exclamation mark glass"
-            className="w-[200px] h-[200px] z-2 md:mt-12 mt-4"
-            width={150}
-            height={150}
+            className="w-[200px] h-[200px] z-2 md:mt-12 mt-4 hidden md:block"
+            width={200}
+            height={200}
+          />
+
+          <Image
+            src="/exclamation-mark-glass.png"
+            alt="exclamation mark glass"
+            className="w-[250px] h-[250px] z-1 absolute -bottom-15 -right-15 md:hidden block opacity-50"
+            width={200}
+            height={200}
           />
         </div>
         <div className="flex-2 flex flex-col gap-4">
@@ -93,9 +102,9 @@ export default function Fitur() {
             <Image
               src="/live-glass.png"
               alt="Live glass"
-              className="w-[200px] h-[200px] z-2 absolute top-0 right-0"
-              width={150}
-              height={150}
+              className="md:w-[200px] md:h-[200px] w-[250px] h-[250px] z-2 absolute md:top-0 md:right-0 -bottom-15 -right-15 md:opacity-100 opacity-50"
+              width={200}
+              height={200}
             />
             <div className="flex flex-col h-full justify-end gap-4">
               <p className="text-2xl font-semibold text-slate-700 w-full z-2">
@@ -121,9 +130,9 @@ export default function Fitur() {
             <Image
               src="/sparkles-glass.png"
               alt="sparkles glass"
-              className="w-[200px] h-[200px] z-2 absolute top-0 right-0"
-              width={150}
-              height={150}
+              className="md:w-[200px] md:h-[200px] w-[250px] h-[250px] z-2 absolute md:top-0 md:right-0 -bottom-15 -right-15 md:opacity-100 opacity-50"
+              width={200}
+              height={200}
             />
             <div className="flex flex-col h-full justify-end gap-4">
               <p className="text-2xl font-semibold text-slate-700 w-full z-2">
